7b4d271f0e6d8c49094eeb1fbbaebec8b8b3b7a1
SafNaaz <iamsafnas@gmail.com>
2020-04-25 16:27:46 +0530

    full done

--- shared/header.js
+++ shared/header.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
+import { isRequired } from 'react-native/Libraries/DeprecatedPropTypes/DeprecatedColorPropType';
 
 export default function Header({ navigation, title }) {
 
@@ -9,12 +10,13 @@ export default function Header({ navigation, title }) {
     }
 
     return (
-        <View style={styles.header}>
+        <ImageBackground source={require('../assets/game_bg.png')} style={styles.header}>
             <MaterialIcons name='menu' size={28} onPress={openMenu} style={styles.icon} />
-            <View>
+            <View style={styles.headerTitle}>
+                <Image source={isRequired('../assets/heart-logo.png')} style={styles.headerImage} />
                 <Text style={styles.headerText}>{title}</Text>
             </View>
-        </View>
+        </ImageBackground>
     )
 }
 
@@ -33,5 +35,13 @@ const styles = StyleSheet.create({
     icon: {
         //position: 'absolute',
         //left: 10
+    },
+    headerTitle: {
+        flexDirection: 'row'
+    },
+    headerImage: {
+        width: 26,
+        height: 26,
+        marginHorizontal: 10
     }
 })
