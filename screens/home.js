3c2d1470a204babbd6df2f1793d8409cfc273c62
SAFNAS <iamsafnas@gmail.com>
2020-06-06 21:59:13 +0530

    made it to work on android emulator

--- screens/home.js
+++ screens/home.js
@@ -1,79 +1,90 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, TouchableOpacity, Modal, TouchableWithoutFeedback, Keyboard } from 'react-native';
-import { globalStyles } from '../styles/global'
-import Card from '../shared/card'
-import { MaterialIcons } from '@expo/vector-icons';
-import ReviewForm from '../screens/reviewForm'
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+  Modal,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import { globalStyles } from "../styles/global";
+import Card from "../shared/card";
+import { MaterialIcons } from "@expo/vector-icons";
+import ReviewForm from "../screens/reviewForm";
 
 export default function Home({ navigation }) {
+  const [modalOpen, setModalOpen] = useState(false);
 
-    const [modalOpen, setModalOpen] = useState(false)
+  const [reviews, setReviews] = useState([
+    { title: "bla bla", rating: 5, body: "lorem ipsum", key: "1" },
+    { title: "blu blu", rating: 4, body: "lorem ipsum", key: "2" },
+    { title: "ble ble", rating: 3, body: "lorem ipsum", key: "3" },
+  ]);
 
-    const [reviews, setReviews] = useState([
-        { title: 'bla bla', rating: 5, body: 'lorem ipsum', key: '1' },
-        { title: 'blu blu', rating: 4, body: 'lorem ipsum', key: '2' },
-        { title: 'ble ble', rating: 3, body: 'lorem ipsum', key: '3' }
-    ])
+  const addReview = (review) => {
+    review.key = Math.random().toString();
+    setReviews((currentReviews) => {
+      return [review, ...currentReviews];
+    });
+    setModalOpen(false);
+  };
 
-    const addReview = (review) => {
-        review.key = Math.random().toString()
-        setReviews((currentReviews) => {
-            return [review, ...currentReviews]
-        })
-        setModalOpen(false)
-    }
-
-    return (
-        <View style={globalStyles.container}>
-            <Modal visible={modalOpen} animationType='slide' >
-                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-                    <View style={styles.modalContent}>
-                        <MaterialIcons
-                            name='close'
-                            size={24}
-                            onPress={() => setModalOpen(false)}
-                            style={{ ...styles.modalToggle, ...styles.modalClose }}
-                        />
-                        <ReviewForm addReview={addReview} />
-                    </View>
-                </TouchableWithoutFeedback>
-            </Modal>
-
+  return (
+    <View style={globalStyles.container}>
+      <Modal visible={modalOpen} animationType="slide">
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.modalContent}>
             <MaterialIcons
-                name='add'
-                size={24}
-                onPress={() => setModalOpen(true)}
-                style={styles.modalToggle}
+              name="close"
+              size={24}
+              onPress={() => setModalOpen(false)}
+              style={{ ...styles.modalToggle, ...styles.modalClose }}
             />
+            <ReviewForm addReview={addReview} />
+          </View>
+        </TouchableWithoutFeedback>
+      </Modal>
 
-            <FlatList
-                data={reviews}
-                renderItem={({ item }) => (
-                    <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
-                        <Card>
-                            <Text style={globalStyles.titleText}>{item.title}</Text>
-                        </Card>
-                    </TouchableOpacity>
-                )}
-            />
-        </View>
-    )
+      <MaterialIcons
+        name="add"
+        size={24}
+        onPress={() => setModalOpen(true)}
+        style={styles.modalToggle}
+      />
+
+      <FlatList
+        data={reviews}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => navigation.navigate("ReviewDetails", item)}
+          >
+            <Card>
+              <Text style={globalStyles.titleText}>{item.title}</Text>
+            </Card>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    modalToggle: {
-        marginBottom: 10,
-        borderWidth: '#f2f2f2',
-        padding: 10,
-        borderRadius: 10,
-        alignItems: 'center',
-        textAlign: 'center'
-    },
-    modalClose: {
-        marginTop: 20,
-        marginBottom: 0
-    },
-    modalContent: {
-        flex: 1
-    }
-})
+  modalToggle: {
+    marginBottom: 10,
+    backgroundColor: "#f2f2f2",
+    padding: 10,
+    borderRadius: 10,
+    alignItems: "center",
+    textAlign: "center",
+  },
+  modalClose: {
+    marginTop: 20,
+    marginBottom: 0,
+    backgroundColor: "#f2f2f2",
+  },
+  modalContent: {
+    flex: 1,
+  },
+});
